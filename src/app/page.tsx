ac4b1cac637aa7f7414c9c3da92d4f6de005247a
Adam Dhayfa Umar <adamhdhayfaumar.aids2022@citchennai.net>
2024-07-31 14:26:25 +0530

    feat/Added talos logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,26 @@ const ParticleText = dynamic(
   () => import("./component/particleText/ParticalText"),
   {
     ssr: false,
-  },
+  }
 );
 
 const Home = () => {
   return (
     <div className="flex-shrink h-screen w-screen overflow-hidden">
+      <link rel="icon" href="/Logo.png" sizes="any" />
       <Navbar />
-      <Canvas className="z-[-1]" camera={{ position: [0, 0, 100], fov: 65 }}>
-        <ParticleText text="Be Vigil" />
-      </Canvas>
+      <Suspense
+        fallback={
+          <div className="flex justify-center h-screen w-screen items-center text-white">
+            Loading...
+          </div>
+        }
+      >
+        <Canvas className="z-[-1]" camera={{ position: [0, 0, 100], fov: 65 }}>
+          <ParticleText text="Be Vigil" />
+        </Canvas>
+      </Suspense>
+
       <Footer />
     </div>
   );
